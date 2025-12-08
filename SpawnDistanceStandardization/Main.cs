e30f2e95a6c4916d34a7ebf9f75c0fa1f0d389a2
TheSamuel17 <samuelseventeen17@gmail.com>
2025-12-08 00:10:04 -0500

    Add project files.

---
+++ SpawnDistanceStandardization/Main.cs
@@ -0,0 +1,96 @@
+using BepInEx;
+using RoR2;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace SpawnDistanceStandardization
+{
+    // Metadata
+    [BepInPlugin("Samuel17.SpawnDistanceStandardization", "SpawnDistanceStandardization", "1.0.0")]
+
+    public class Main : BaseUnityPlugin
+    {
+        // Master prefabs to be set to their respective distances.
+        public static List<GameObject> masterPrefabsFar = new() {};
+        public static List<GameObject> masterPrefabsStandard = new() {};
+        public static List<GameObject> masterPrefabsClose = new() {};
+
+        // Config fields
+        public static string farSpawns = "GupMaster, JellyfishMaster, AcidLarvaMaster, MagmaWormMaster, ElectricWormMaster";
+        public static string standardSpawns = "BeetleMaster";
+        public static string closeSpawns = "";
+
+        public void Awake()
+        {
+            // Logging!
+            Log.Init(Logger);
+
+            // Load configs
+            farSpawns = Config.Bind("Spawn Distances", "Far", "GupMaster, JellyfishMaster, AcidLarvaMaster, MagmaWormMaster, ElectricWormMaster", "Specify the monsters that should be set to Far (70-120m) by entering their internal master names.\nMake sure to separate them with commas.").Value;
+            standardSpawns = Config.Bind("Spawn Distances", "Standard", "BeetleMaster", "Specify the monsters that should be set to Standard (25-40m) by entering their internal master names.\nMake sure to separate them with commas.").Value;
+            closeSpawns = Config.Bind("Spawn Distances", "Close", "", "Specify the monsters that should be set to Close (8-20m) by entering their internal master names.\nMake sure to separate them with commas.").Value;
+
+            // Sort configs
+            RoR2Application.onLoad += () =>
+            {
+                SortConfigs(farSpawns, masterPrefabsFar, "Far");
+                SortConfigs(standardSpawns, masterPrefabsStandard, "Standard");
+                SortConfigs(closeSpawns, masterPrefabsClose, "Close");
+            };    
+
+            // Adjust spawn distances
+            On.RoR2.ClassicStageInfo.RebuildCards += AdjustSpawnDistances;
+        }
+
+        private void SortConfigs(string spawns, List<GameObject> listPrefabs, string distance)
+        {
+            spawns = new string(spawns.ToCharArray().Where(c => !char.IsWhiteSpace(c)).ToArray());
+            string[] monsters = spawns.Split(',');
+
+            foreach (string str in monsters)
+            {
+                GameObject masterPrefab = MasterCatalog.FindMasterPrefab(str);
+                if (masterPrefab)
+                {
+                    listPrefabs.Add(masterPrefab);
+                    Log.Message(masterPrefab.name + " has been added to the " + distance + " spawn list.");
+                }
+            }
+        }
+
+        private void AdjustSpawnDistances(On.RoR2.ClassicStageInfo.orig_RebuildCards orig, ClassicStageInfo self, DirectorCardCategorySelection forcedMonsterCategory = null, DirectorCardCategorySelection forcedInteractableCategory = null)
+        {
+            orig(self, forcedMonsterCategory, forcedInteractableCategory);
+
+            // Skip if Void Fields, they're all set to spawn far by default
+            if (SceneCatalog.mostRecentSceneDef != SceneCatalog.FindSceneDef("arena"))
+            {
+                List<MasterCatalog.MasterIndex> list = new List<MasterCatalog.MasterIndex>(self.monsterSelection.choices.Length);
+                for (int i = 0; i < self.monsterSelection.Count; i++)
+                {
+                    DirectorCard directorCard = self.monsterSelection.choices[i].value;
+                    SpawnCard spawnCard = directorCard.GetSpawnCard();
+                    if (spawnCard)
+                    {
+                        if (masterPrefabsFar.Contains(spawnCard.prefab)) // Set to "Far"
+                        {
+                            Log.Message(spawnCard.prefab.name + " spawn distance set to Far.");
+                            directorCard.spawnDistance = DirectorCore.MonsterSpawnDistance.Far;
+                        }
+                        else if (masterPrefabsStandard.Contains(spawnCard.prefab)) // Set to "Standard"
+                        {
+                            Log.Message(spawnCard.prefab.name + " spawn distance set to Standard.");
+                            directorCard.spawnDistance = DirectorCore.MonsterSpawnDistance.Standard;
+                        }
+                        else if (masterPrefabsClose.Contains(spawnCard.prefab)) // Set to "Close"
+                        {
+                            Log.Message(spawnCard.prefab.name + " spawn distance set to Close.");
+                            directorCard.spawnDistance = DirectorCore.MonsterSpawnDistance.Close;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Thunderstore/README.md
@@ -0,0 +1,25 @@
+# SpawnDistanceStandardization
+
+Some enemies spawn a little close for comfort, some spawn weirdly far and some can't make up their mind. This mod exists to fix that.
+
+Default settings:
+* Jellyfish are set to "Far" on every stage. This is the standard for them, but some stages (e.g. Helminth Hatchery) omit this, which makes them notably harder to react to.
+* Magma & Overloading Worms are set to "Far" on every stage, because I don't like them spawning right underneath me. This is how they're handled in Rallypoint Delta.
+* Beetles are set to "Standard" on every stage. This is for Wetland Aspect specifically; I don't know why they spawn extra far there.
+* Gups are set to "Far" on every stage, to combat their very short and sudden spawning animation.
+* Larvae are set to "Far" on every stage, because fuck them.
+
+Configs will let you adjust the settings for just about anything that can spawn in the game.
+
+## Mod Compatibility & Recommendations
+
+Enemies with the "Far" setting have a tendency to idle about, because they will often end up out of sight. To combat this, I recommend [TweakAI](https://thunderstore.io/package/Nuxlar/TweakAI/) to allow them to path to you without incident. Besides, I think enemies targeting you more proactively is a neat change in general.
+
+## Credits
+
+* Moffein & Chinchi, for writing helpful and sometimes yoinkable code.
+* The denizens of `#development`, for answering my questions.
+
+## Contact
+
+Direct your feedback & bug reports to `samuel17` on Discord, either through DMs or the Risk of Rain 2 Modding Discord.
